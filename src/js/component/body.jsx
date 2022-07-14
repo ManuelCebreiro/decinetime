57a7c9299ae80d0877f65feda2f3a9be42028402
ManuelCebreiro <cebreirom@gmail.com>
2022-07-14 09:17:35 +0000

    probando

---
+++ src/js/component/body.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./body.css"
+
+
+const Body = () => {
+    return (
+        <div className="row">
+            <div className="col-3"></div>
+            <div className="col-6 m-0 vh-100 d-flex justify-content-center align-items-center">
+                <div className="bodyfilm1"></div>
+                <div className="my-4"></div>
+                <div className="bodyfilm2"></div>
+            </div>
+            <div className="col-3"></div>
+
+        </div>
+    );
+
+
+}
+
+export default Body;
